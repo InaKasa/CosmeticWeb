2571db893b112bbd23ea5a043d141e294645e624
InaKasa <121641517+InaKasa@users.noreply.github.com>
2023-01-24 21:28:13 +0100

    initial

--- CosmeticWeb/Program.cs
+++ CosmeticWeb/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
